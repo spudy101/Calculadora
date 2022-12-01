22a2fcb496b6996ec9a0f442ab380ca5fc229d0c
Spudy101 <camilosantanderh@ghmail.com>
2022-12-01 17:08:08 -0300

    nueva calculadora

---
+++ Calculadora.js
@@ -0,0 +1,25 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+    exponente(num1, num2) {
+        return num1 ** num2;
+    }
+
+    sacarResto(num1, num2) {
+        return num1 % num2;
+    }
+} 
